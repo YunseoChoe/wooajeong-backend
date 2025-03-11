1c36f873fb696cd3a67e5a0dfbc1110b454cbbfc
choeyunseo <choeyunseo4060@naver.com>
2025-03-11 20:13:14 +0900

    Update HelloController

--- src/main/java/com/example/capstone/controller/HelloController.java
+++ src/main/java/com/example/capstone/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String helloWorld() {
-        return "hi hi";
+        return "hi there";
     }
 }
