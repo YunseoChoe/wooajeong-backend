e0d673e19cb25e062ef36b4224006d133dea74d1
choeyunseo <choeyunseo4060@naver.com>
2025-04-13 23:14:39 +0900

    [#2] Feat: 채팅 기능 테스트

--- src/main/java/com/example/capstone/repository/TestRepository.java
+++ src/main/java/com/example/capstone/repository/TestRepository.java
@@ -3,9 +3,7 @@ package com.example.capstone.repository;
 import com.example.capstone.domain.ChattingContent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
 // 채팅방 (roomId)을 기준으로 메세지 불러오기
-public interface ChattingContentRepository extends MongoRepository<ChattingContent, String> {
-    ChattingContent findChattingContentByRoomId(int roomId);
+public interface TestRepository extends MongoRepository<ChattingContent, String> {
+    ChattingContent findChattingContentByName(String name);
 }
--- src/main/java/com/example/capstone/service/ChatMessageReceiver.java
+++ src/main/java/com/example/capstone/service/ChatMessageReceiver.java
@@ -1,4 +1,25 @@
-package com.example.capstone.service;
-
-public class ChatMessageReceiver {
-}
+//package com.example.capstone.service;
+//
+//import com.example.capstone.dto.ChatMessageDto;
+//import com.example.capstone.handler.ChatWebSocketHandler;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.stereotype.Service;
+//
+//import java.io.IOException;
+//
+//// RabbitMQ에서 메세지 수신 (Consumer역할: 특정 Queue를 구독하고, 받은 메세지를 Websocket으로 유저에게 전달)
+//@Service
+//public class ChatMessageReceiver {
+//
+//    private final ChatWebSocketHandler handler;
+//
+//    public ChatMessageReceiver(ChatWebSocketHandler handler) {
+//        this.handler = handler;
+//    }
+//
+//    // Queue 구독
+//    @RabbitListener(queues = "chat.queue.user1") // 예시: userB만 처리
+//    public void receive(ChatMessageDto message) throws IOException {
+//        handler.sendToUser(Integer.parseInt(message.getReceiverId()), message);
+//    }
+//}
