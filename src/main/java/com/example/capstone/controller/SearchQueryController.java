43050e202ce5d7d4e3d562b491f682265fd09a08
choeyunseo <choeyunseo4060@naver.com>
2025-04-28 23:48:04 +0900

    Feat: 쿼리문 반환 API 구현

--- src/main/java/com/example/capstone/controller/SearchQueryController.java
+++ src/main/java/com/example/capstone/controller/SearchQueryController.java
@@ -4,13 +4,12 @@ import com.example.capstone.domain.SearchQuery;
 import com.example.capstone.repository.SearchQueryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/searches/save")
+@RequestMapping("/searches")
 public class SearchQueryController {
 
     private final SearchQueryRepository searchQueryRepository;
@@ -20,9 +19,15 @@ public class SearchQueryController {
         this.searchQueryRepository = searchQueryRepository;
     }
 
-    @PostMapping
+    @PostMapping("/save")
     public ResponseEntity<SearchQuery> saveQuery(@RequestBody SearchQuery searchQuery) {
         SearchQuery savedQuery = searchQueryRepository.save(searchQuery);
         return ResponseEntity.ok(savedQuery);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<SearchQuery>> getAllQueries() {
+        List<SearchQuery> queries = searchQueryRepository.findAll();
+        return ResponseEntity.ok(queries);
     }
 }
