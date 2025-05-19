f0260ef58f01fc5a59b6e6ea6ea7bb8c395a9d18
choeyunseo <choeyunseo4060@naver.com>
2025-05-19 20:03:46 +0900

    배포 테스트

--- src/main/java/com/example/capstone/repository/ChatMessageRepository.java
+++ src/main/java/com/example/capstone/repository/ChatMessageRepository.java
@@ -8,3 +8,4 @@ import java.util.List;
 public interface ChatMessageRepository extends MongoRepository<ChattingContent, String> {
     List<ChattingContent> findByRoomIdOrderBySendTimeAsc(String roomId); // room에 해당하는 모든 메세지를 오름차순 정렬 후 반환.
 }
+
